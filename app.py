c15c6b3e117e4913c678ab7077664150d2ca7d29
CooperativeArts <dmcooperativearts@gmail.com>
2024-11-29 19:30:01 +1100

    regression to app and req

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from flask import Flask
-import openai
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
-openai.api_key = os.getenv('OPENAI_API_KEY')
 
 @app.route('/')
 def hello():
@@ -14,7 +8,7 @@ def hello():
 
 @app.route('/test')
 def test():
-    return "Basic test route!"  # Added exclamation mark
+    return "Basic test route"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
