4eff7826006d6e7c403af8b5544b8f9aac8a3e14
CooperativeArts <dmcooperativearts@gmail.com>
2024-11-29 19:36:33 +1100

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
+@app.route('/test')
+def test():
+    return "Basic test route"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
